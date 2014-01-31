ba428ee43da7cdc574df06d501bea93f10696ba8
swhallee <swhallee@gmail.com>
2014-01-30 19:45:52 -0500

    PLEASE HELP

--- include/physics.h
+++ include/physics.h
@@ -1,15 +1,15 @@
+#ifndef _PHYSICS
+#define _PHYSICS
+
 //#include "physics.h"
-#include "VECTOR3.h"
+#include <VECTOR3.h>
 #include <iostream>
 
-class nBody {
-        public:
-                nBody();
-                ~nBody();
-		Vector3f vecAcc(Vector3f r1, Vector3f r2, float m_1) {}
-		Vector3f vecVel(Vector3f r1, Vector3f r2, Vector3f v1, Vector3f a1) {}
-		Vector3f vecPos(Vector3f r1, Vector3f r2, Vector3f v1, Vector3f a1) {}
-};
+namespace nBody {
+	inline Vector3f vecAcc( Vector3f r1, Vector3f r2, float m_1 ) {};
+	inline Vector3f vecVel(/*Vector3f r1, Vector3f r2, Vector3f v1, Vector3f a1*/) {};
+	inline Vector3f vecPos(/*Vector3f r1, Vector3f r2, Vector3f v1, Vector3f a1*/) {};
+} //namespace nBody
 
 /*
 int main() {
